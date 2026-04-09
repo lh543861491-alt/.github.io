046c3004949a378af395431b79abb9788dec7bad
lh543861491-alt <lh543861491@gmail.com>
2026-04-09 20:59:07 +0800

    Add files via upload

---
+++ amazon-portfolio/js/jsmain.js
@@ -0,0 +1,9 @@
+// 平滑滚动
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
